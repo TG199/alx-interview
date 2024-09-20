121364a9495461e63fbba0e49c72a31a92c66065
Kelechi Ebiri <ebirikele@gmail.com>
2024-09-20 23:54:50 +0100

    use request 2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,30 @@
 
 const request = require('request');
 
-// Function to fetch movie data and characters
-async function fetchCharacters (movieId) {
-  try {
-    // Fetch the movie details from the SWAPI using the movie ID
-    const movieData = await request(`https://swapi.dev/api/films/${movieId}/`);
-    const movie = JSON.parse(movieData);
-    const characters = movie.characters;
-
-    // Fetch and print each character's name
-    for (const characterUrl of characters) {
-      const characterData = await request(characterUrl);
-      const character = JSON.parse(characterData);
-      console.log(character.name);
-    }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
-// Get the Movie ID from the command line arguments
 const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-// Check if Movie ID is provided
-if (!movieId) {
-  console.error('Please provide a Movie ID as the first argument.');
-  process.exit(1);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-// Fetch and display the characters for the given Movie ID
-fetchCharacters(movieId);
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
